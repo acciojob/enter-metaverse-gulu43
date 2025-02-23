647dd2bfad7e285b5e88626d5655be4b574268d9
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-23 11:08:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn = document.getElementById('enterBtn');
+btn.addEventListener('click',function (event) {
+    let p_val = document.getElementById('status');
+    p_val.innerHTML='<h1>Enter Metaverse</h1>';
+})
